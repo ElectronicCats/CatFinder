20433739eab4d744a6daade1dc2cecec40346660
Rocio <rocio95cua>
2019-08-13 17:13:13 -0500

    add read CCS811

--- Software/Sensors_Bast/Sensors_Bast.ino
+++ Software/Sensors_Bast/Sensors_Bast.ino
@@ -1,33 +1,65 @@
 /*program Bast Read I2C sensors*/
 #include <Wire.h>
 #include "SparkFunBME280.h"
-//#define Serial SerialUSB
+#include "Adafruit_CCS811.h"
+
+//Variables BME280
 float humidity=0;
 float pressure=0;
 float temp=0;
 
-//BME280 mySensorA; //Uses default I2C address 0x77
-BME280 mySensorB; //Uses I2C address 0x76 (jumper closed)
+//Variables CCS811
+float co2=0;
+float tvoc=0;
+
+
+BME280 mySensorB;//Object BME280
+Adafruit_CCS811 ccs; //Object CCS811
 
 void setup()
 {
   Serial.begin(115200);
+  Wire.begin(0,16);
   while(!Serial);
-  Serial.println("Example showing alternate I2C addresses");
 
- // Wire.begin(4,5);
+  //BME280 CONFIG
   mySensorB.setI2CAddress(0x76); //Connect to a second sensor
   if(mySensorB.beginI2C() == false) Serial.println("Sensor B connect failed");
+
+  //CCS811 config
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+   //calibrate temperature sensor CCS811
+  while(!ccs.available());
+  float temp = ccs.calculateTemperature();
+  ccs.setTempOffset(temp - 25.0);
 }
 
 void loop()
 {
+  //Variables BME280
   humidity=mySensorB.readFloatHumidity();
   pressure=mySensorB.readFloatPressure();
   temp=mySensorB.readTempC();
-  Serial.println(humidity);
-  Serial.println(pressure);
-  Serial.println(temp);
-  
+
+    if(ccs.available()){
+    float tempCCS = ccs.calculateTemperature();
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      co2=ccs.geteCO2();
+      Serial.print(co2);
+      Serial.print("ppm, TVOC: ");
+      tvoc=ccs.getTVOC();
+      Serial.print(tvoc);
+      Serial.print("ppb   Temp:");
+      Serial.println(tempCCS);
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
   delay(1000);
 }
