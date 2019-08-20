6e4723d487cbebd618d35d3692459b43da222ee2
Rocio <rocio95cua>
2019-08-19 23:34:30 -0500

    Add reads GY-87

--- Software/Sensors_Bast/Sensors_Bast.ino
+++ Software/Sensors_Bast/Sensors_Bast.ino
@@ -2,6 +2,11 @@
 #include <Wire.h>
 #include "SparkFunBME280.h"
 #include "Adafruit_CCS811.h"
+#include <MPU6050.h>//acelerometro y gyroscopio
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>//Magnetometro
+
+#define Serial SerialUSB
 
 //Variables BME280
 float humidity=0;
@@ -16,10 +21,17 @@ float tvoc=0;
 BME280 mySensorB;//Object BME280
 Adafruit_CCS811 ccs; //Object CCS811
 
+MPU6050 accelgyro;
+//Variables de Aceleración y Giroscopio
+//Accel and Gyro Vars
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(1);//Magnetometro
+
 void setup()
 {
   Serial.begin(115200);
-  Wire.begin(0,16);
   while(!Serial);
 
   //BME280 CONFIG
@@ -35,14 +47,34 @@ void setup()
   while(!ccs.available());
   float temp = ccs.calculateTemperature();
   ccs.setTempOffset(temp - 25.0);
-}
 
+  //Inicializar acelerometro y gyroscopio
+  accelgyro.initialize();  /// Initialize MPU
+
+  accelgyro.setI2CMasterModeEnabled(false);
+  accelgyro.setI2CBypassEnabled(true) ;
+  accelgyro.setSleepEnabled(false);
+
+  // Initialise the sensor
+  if (!mag.begin())
+  {
+    // There was a problem detecting the HMC5883 ... check your connections
+    Serial.println(F("Ooops, no HMC5883 detected ... Check your wiring!"));
+    while (1);
+  }
+}
 void loop()
 {
   //Variables BME280
   humidity=mySensorB.readFloatHumidity();
   pressure=mySensorB.readFloatPressure();
   temp=mySensorB.readTempC();
+  Serial.print("humidity:");
+  Serial.println(humidity);
+  Serial.print("pressure:");
+  Serial.println(pressure);
+  Serial.print("temp:");
+  Serial.println(temp);
 
     if(ccs.available()){
     float tempCCS = ccs.calculateTemperature();
@@ -61,5 +93,23 @@ void loop()
       while(1);
     }
   }
+
+   sensors_event_t event;
+  // Display the results (magnetic vector values are in micro-Tesla (uT))
+  Serial.print(F("Magnetometro:  "));
+  Serial.print(F("X: ")); Serial.print(event.magnetic.x); Serial.print(F("  "));
+  Serial.print(F("Y: ")); Serial.print(event.magnetic.y); Serial.print(F("  "));
+  Serial.print(F("Z: ")); Serial.print(event.magnetic.z); Serial.print(F("  ")); Serial.println(F("uT"));
+
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print(F("Acelerometro "));
+  Serial.print(F("X:")); Serial.print(ax); Serial.print("\t");
+  Serial.print(F("Y:")); Serial.print(ay); Serial.print("\t");
+  Serial.print(F("Z:")); Serial.print(az); Serial.print("\n");
+  Serial.print(F("Giroscopio "));
+  Serial.print(F("X:")); Serial.print(gx); Serial.print("\t");
+  Serial.print(F("X:")); Serial.print(gy); Serial.print("\t");
+  Serial.print(F("X:")); Serial.println(gz); Serial.print("\n");
+  
   delay(1000);
 }
