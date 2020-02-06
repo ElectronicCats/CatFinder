86e33755c640646ed73cc11eaf56c12cfd5ea880
Eduardo <werowero.edy@gmail.com>
2020-02-06 16:29:16 -0600

    Add program for Bast Pro mini to update the ESP32

---
+++ Firmware/bastAsProgrammer/bastAsProgrammer.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+}
+
+void loop() {
+  if (Serial.available()) {      // If anything comes in Serial (USB),
+    Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
+  }
+
+  if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
+    Serial.write(Serial1.read());   // read it and send it out Serial (USB)
+  }
+}
